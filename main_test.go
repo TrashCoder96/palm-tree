892c1dfa0054f92665ee8ef9198c36aacaab4966
Ivan Timofeev <kiberaction@yandex.ru>
2019-12-22 00:19:29 +0300

    add DeleteFromLeafNode tests

--- main_test.go
+++ main_test.go
@@ -68,14 +68,71 @@ func TestInsertToLeafNode_atEndPosition(t *testing.T) {
 	}
 }
 
+func TestInsertToLeafNode_emptyList(t *testing.T) {
+	node := initOneTestLeafNode(0)
+	node.insertToLeafNode(100, "")
+	if !(node.leafHead != nil && node.leafHead.value == 100 && node.leafHead.nextKey == nil) {
+		t.FailNow()
+	}
+}
+
+func TestDeleteFromLeafNode_ok(t *testing.T) {
+	node := initOneTestLeafNode(9)
+	node.deleteFromLeafNode(40)
+	keys := []int64{10, 20, 30, 50, 60, 70, 80, 90}
+	currentKey := node.leafHead
+	for _, key := range keys {
+		if currentKey.value != key {
+			t.FailNow()
+		}
+		currentKey = currentKey.nextKey
+	}
+}
+
+func TestDeleteFromLeafNode_atStartPosition(t *testing.T) {
+	node := initOneTestLeafNode(9)
+	node.deleteFromLeafNode(10)
+	keys := []int64{20, 30, 40, 50, 60, 70, 80, 90}
+	currentKey := node.leafHead
+	for _, key := range keys {
+		if currentKey.value != key {
+			t.FailNow()
+		}
+		currentKey = currentKey.nextKey
+	}
+}
+
+func TestDeleteFromLeafNode_atEndPosition(t *testing.T) {
+	node := initOneTestLeafNode(9)
+	node.deleteFromLeafNode(90)
+	keys := []int64{10, 20, 30, 40, 50, 60, 70, 80}
+	currentKey := node.leafHead
+	for _, key := range keys {
+		if currentKey.value != key {
+			t.FailNow()
+		}
+		currentKey = currentKey.nextKey
+	}
+}
+
+func TestDeleteFromLeafNode_emptyList(t *testing.T) {
+	node := initOneTestLeafNode(0)
+	success := node.deleteFromLeafNode(90)
+	if !(node.leafHead == nil && !success) {
+		t.FailNow()
+	}
+}
+
 func initOneTestLeafNode(countOfKeys int) *BPlusTreeNode {
 	value := int64(10)
 	node := BPlusTreeNode{
 		isLeaf:      true,
 		countOfKeys: countOfKeys,
-		leafHead: &bPlusTreeKey{
+	}
+	if countOfKeys > 0 {
+		node.leafHead = &bPlusTreeKey{
 			value: value,
-		},
+		}
 	}
 	previousKey := node.leafHead
 	for i := 1; i < countOfKeys; i++ {
